67a4383fb99463f06a6503e98bc0c570eec8c1db
Mike O'Brien <mikeob9@gmail.com>
2020-07-09 18:47:00 -0400

    Find transitions between stations.

--- Code/epistemic_error.R
+++ Code/epistemic_error.R
@@ -1,15 +1,38 @@
-library(TelemetryR)
-
-dat <- vemsort('p:/obrien/biotelemetry/detections/dnr/marshyhope')
+## Attempting to re-create analyses found in Bruneel et al. 2020
+## https://doi.org/10.1016/j.ecoinf.2020.101133
+##  Code is embargoed until 2020-07-17
 
 library(data.table)
-dat <- setDT(dat)
 
-dat <- dat[transmitter %in% paste0('A69-9001-', c(seq(21063, 21072, 1),
-                                                  seq(23900, 23904, 1),
-                                                  seq(26350, 26354, 1),
-                                                  seq(27543, 27547, 1),
-                                                  seq(18009, 18010, 1),
-                                                  seq(18977, 18979, 1)))]
 
-dat <- dat[, year := year(date.local)]
+# Import and munge data ----
+dets <- fread('data/derived/sturgeon_detections.gz')
+
+## Will have to run this by year, as there are a different number of receivers
+dets <- dets[, year := year(date.local)]
+
+
+## TRIAL RUN: SELECT ONLY 2017 ##
+dets <- dets[year == 2017]
+##
+
+
+
+# Identify when fish move to a different station ----
+##  Order each transmitter from earliest to latest detection
+setorder(dets, transmitter, date.local)
+
+##  Make a dummy column of the stations shifted up one ("shift()")
+##  "by = transmitter" makes sure each transmitter "shifts" separately
+##  Will need to add year to "by = " when running multiple years
+dets <- dets[, next_station := shift(station, fill = '-999'), by = transmitter]
+
+##  Select detections where the next station is not the same as the current station
+dets <- dets[station != next_station]
+
+##  Drop "next_station" column
+dets <- dets[, next_station := NULL]
+
+
+
+# Find distances between sites ----
