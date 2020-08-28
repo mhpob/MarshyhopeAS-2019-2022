6f26308ffd32043ceafc4f0ae56029e81e71c5b1
Mike O'Brien <mikeob9@gmail.com>
2020-08-28 12:04:44 -0400

    Simplify?

--- Code/nanticoke rkm.R
+++ Code/nanticoke rkm.R
@@ -1,25 +1,50 @@
-library(sf)
+library(dplyr); library(sf)
 
 # Read and manipulate flowline data ---
 flowline <- st_read('data/raw/nanticoke_flowline.gpkg')
 
 
-##  Merge different river sections into one.
-flowline <- st_combine(flowline)
-flowline <- st_line_merge(flowline)
+##  Merge different within-river secctions into one.
+flowline <- flowline %>%
+
+  # Combine different sections into one object
+  group_by(gnis_name) %>%
+  summarize(geom = st_combine(geom)) %>%
+
+  # Merge within-object lines together
+  group_by(gnis_name) %>%
+  summarize(geom = st_line_merge(geom)) %>%
+  st_transform(32618)
 
 
-# Reproject spatial object to something measured in meters----
-flowline <- st_transform(flowline, 32618)
+
+# Calculate RKM of Creek mouths ----
+## Find where the creeks meet the mainstem
+mouths <- flowline %>%
+  st_intersection() %>%
+  filter(n.overlaps == 2)
+
+## Find RKM of Creek mouths
+nan_split <- flowline %>%
+  filter(gnis_name == 'Nanticoke River') %>%
+
+  # Split Nanticoke by mouth locations
+  lwgeom::st_split(mouths) %>%
+
+  # Pull out the split sections
+  st_collection_extract('LINESTRING') %>%
+
+  # Find their lengths in km
+  st_length() %>%
+  units::set_units(km)
+
+## Return RKMs
+cumsum(nan_split)
+
+# Units: [km]
+# [1] 45.55766 58.47172 68.09327 80.51762
+#     Marshy,  Broad,   Deep
 
 
 
-# Break the line that makes up the flowline into points 1 km apart from each other ----
-rkms <- flowline %>%
-  st_line_sample(n = as.numeric(st_length(flowline)/1000)) %>%
-  st_cast('POINT') %>%
-  st_transform(4326) %>%
-  st_coordinates() %>%
-  as.data.frame
-
-write.csv(rkms, 'data/raw/nanticoke_rkms.csv')
+#  ----
