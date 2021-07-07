8f065808556829c7c6618462ffb0bde61e11eac9
Mike O'Brien <mikeob9@gmail.com>
2021-07-07 12:21:09 -0400

    Working to get sync model to converge.

--- Code/yaps_trial.R
+++ Code/yaps_trial.R
@@ -22,8 +22,9 @@ detections <- prepDetections_gsub(vue, 'vemco_vue')
 detections[is.na(frac), frac := 0]
 
 # Remove detections after VR2ARs started to be pulled
-detections <- detections[ts <= '2020-11-03 16:00:00']
-
+# detections <- detections[ts <= as.POSIXct('2020-11-03 00:00:00', tz = 'UTC')]
+# detections <- det[ts %between% c(as.POSIXct('2020-09-01 00:00:00', tz = 'UTC'),
+                                 # as.POSIXct('2020-10-01 00:00:00', tz = 'UTC'))]
 
 library(readxl)
 excel_sheets('data/raw/vps-nanticokeriver-brookview-01.xls')
@@ -33,10 +34,11 @@ hydro_loc <- read_excel('data/raw/vps-nanticokeriver-brookview-01.xls',
                         skip = 1)
 
 # Remove Brookview stations
-hydro_loc <- hydro_loc[-c(1:4),]
+hydro_loc <- hydro_loc[-c(1,3),]
+
 hydro_loc <- st_as_sf(hydro_loc, coords = c('Longitude', 'Latitude'), crs = 4326,
                       remove = F) %>%
-  st_transform('+proj=aeqd +lat_0=38.574224 +lon_0=-75.789577 +x_0=750 +y_0=750 +ellps=WGS84')
+  st_transform('+proj=aeqd +lat_0=38.574224 +lon_0=-75.789577 +ellps=WGS84')
 
 setDT(hydro_loc)
 
@@ -44,9 +46,9 @@ hydro_ser <- read_excel('data/raw/vps-nanticokeriver-brookview-01.xls',
                         'Stations',
                         skip = 1)
 setDT(hydro_ser)
-# hydro_ser[2:3, `End Time` := hydro_ser$`End Time`[4:5]]
-# hydro_ser <- hydro_ser[-c(1, 4:5),]
-hydro_ser <- hydro_ser[-c(1:7),]
+hydro_ser[2:3, `End Time` := hydro_ser$`End Time`[4:5]]
+hydro_ser <- hydro_ser[-c(1, 4:5),]
+# hydro_ser <- hydro_ser[-c(1:7),]
 hydro_ser[, Name := Name[nafill(replace(.I, is.na(Name), NA), "locf")]]
 hydro_ser[, Latitude := Latitude[nafill(replace(.I, is.na(Latitude), NA), "locf")]]
 hydro_ser[, Longitude := Longitude[nafill(replace(.I, is.na(Longitude), NA), "locf")]]
@@ -65,7 +67,7 @@ detections <- foverlaps(detections, key,
 
 hydro_ser <- hydro_ser %>%
   st_as_sf(coords = c('Longitude', 'Latitude'), crs = 4326, remove = F) %>%
-  st_transform('+proj=aeqd +lat_0=38.574224 +lon_0=-75.789577 +x_0=750 +y_0=750 +ellps=WGS84') %>%
+  st_transform('+proj=aeqd +lat_0=38.574224 +lon_0=-75.789577 +ellps=WGS84') %>%
   data.table()
 hydro_ser[, ':='(X = st_coordinates(geometry)[, 1],
                  Y = st_coordinates(geometry)[, 2],
@@ -80,8 +82,6 @@ hydros <- data.table(
   hydro_ser[grepl('^A', Device), .(sync_tag = as.numeric(gsub('.*-', '', Device)))]
 )
 
-ex <- list(hydros = copy(hydros),
-           detections = copy(detections))
 
 
 wtemp <- fread('data/raw/marshyhopevps_event_export.csv',
@@ -92,21 +92,98 @@ wtemp <- wtemp[, .(bt = mean(data)),
 wtemp[, ':='(ts = dateandtimeutc,
              ss = tempToSs(temp = bt, sal = 0, depth = 5))]
 
+ex <- list(hydros = copy(hydros)[, .(serial, x, y, z, sync_tag, idx)],
+           detections = copy(detections)[
+             tag %in% hydros$sync_tag &
+               ts <= as.POSIXct('2020-10-12 00:00:00', tz = 'UTC'),
+             .(ts, tag, epo, frac, serial)],
+           ss = copy(wtemp)[ts <= as.POSIXct('2020-10-12 00:00:00', tz = 'UTC'),
+                            .(ts, ss)])
+
 
 k <- getInpSync(sync_dat = ex,
-                max_epo_diff = 250,
-                min_hydros = 2,
-                time_keeper_idx = 2,
-                fixed_hydros_idx = 1:10,
+                max_epo_diff = 230,
+                min_hydros = 5,
+                time_keeper_idx = 4, #546211 had the lowest drift (~1s)
+                fixed_hydros_idx = 3:12,
                 n_offset_day = 2,
                 n_ss_day = 2,
-                keep_rate = 100,
-                excl_self_detect = F,
+                keep_rate = 165,
+                excl_self_detect = T,
                 ss_data_what = 'data',
-                ss_data = wtemp
+                ss_data = ex$ss
                 )
 
 getSyncCoverage(k, plot=TRUE)
 
+sync_model <- getSyncModel(k, silent=F, tmb_smartsearch = T, max_iter = 500)
 
-sync_model <- getSyncModel(k, silent=F, max_iter=500, tmb_smartsearch = T)
+plotSyncModelHydros(sync_model)
+
+plotSyncModelResids(sync_model, by = "overall")
+plotSyncModelResids(sync_model, by = "quantiles")
+plotSyncModelResids(sync_model, by = "sync_tag")
+plotSyncModelResids(sync_model, by = "hydro")
+plotSyncModelResids(sync_model, by = "temporal_hydro")
+plotSyncModelResids(sync_model, by = "temporal_sync_tag")
+
+j <- fineTuneSyncModel(sync_model, eps_threshold=1E4, silent=TRUE)
+j <- fineTuneSyncModel(j, eps_threshold=1E3, silent=TRUE)
+
+plotSyncModelResids(j, by = "overall")
+plotSyncModelResids(j, by = "quantiles")
+plotSyncModelResids(j, by = "sync_tag")
+plotSyncModelResids(j, by = "hydro")
+plotSyncModelResids(j, by = "temporal_hydro")
+plotSyncModelResids(j, by = "temporal_sync_tag")
+plotSyncModelHydros(j)
+plotSyncModelCheck(j, by = "hydro")
+plotSyncModelCheck(j, by = "sync_tag")
+plotSyncModelCheck(j, by = "sync_bin_sync")
+plotSyncModelCheck(j, by = "sync_bin_hydro")
+
+
+jj <- fineTuneSyncModel(j, eps_threshold=1E2, silent=TRUE)
+plotSyncModelResids(jj, by = "overall")
+plotSyncModelResids(jj, by = "quantiles")
+plotSyncModelResids(jj, by = "sync_tag")
+plotSyncModelResids(jj, by = "hydro")
+plotSyncModelResids(jj, by = "temporal_hydro")
+plotSyncModelResids(jj, by = "temporal_sync_tag")
+plotSyncModelHydros(jj)
+plotSyncModelCheck(jj, by = "hydro")
+plotSyncModelCheck(jj, by = "sync_tag")
+plotSyncModelCheck(jj, by = "sync_bin_sync")
+plotSyncModelCheck(jj, by = "sync_bin_hydro")
+
+
+k3 <- getInpSync(sync_dat = ex,
+                max_epo_diff = 250,
+                min_hydros = 3,
+                time_keeper_idx = 2, #546211 had the lowest drift (~1s)
+                fixed_hydros_idx = 1:10,
+                n_offset_day = 2,
+                n_ss_day = 2,
+                keep_rate = 85,
+                excl_self_detect = F,
+                ss_data_what = 'data',
+                ss_data = ex$ss
+)
+
+getSyncCoverage(k2, plot=TRUE)
+sync_model <- getSyncModel(k2, silent=F, max_iter = 500, tmb_smartsearch = T)
+
+
+plotSyncModelResids(sync_model, by = "overall")
+plotSyncModelResids(sync_model, by = "quantiles")
+plotSyncModelResids(sync_model, by = "sync_tag")
+plotSyncModelResids(sync_model, by = "hydro")
+plotSyncModelResids(sync_model, by = "temporal_hydro")
+plotSyncModelResids(sync_model, by = "temporal_sync_tag")
+plotSyncModelHydros(sync_model)
+plotSyncModelCheck(sync_model, by = "hydro")
+plotSyncModelCheck(sync_model, by = "sync_tag")
+plotSyncModelCheck(sync_model, by = "sync_bin_sync")
+plotSyncModelCheck(sync_model, by = "sync_bin_hydro")
+
+j <- fineTuneSyncModel(sync_model, eps_threshold=1E4, silent=F)
