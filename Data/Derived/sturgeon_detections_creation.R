40c2e91df099e2ac2c44d78aaf0f662ece062dd8
Mike O'Brien <mikeob9@gmail.com>
2022-01-03 20:42:32 -0500

    Repair station names.

--- Data/Derived/sturgeon_detections_creation.R
+++ Data/Derived/sturgeon_detections_creation.R
@@ -44,17 +44,65 @@ detections <- detections[transmitter %in%
            10157,
            seq(21060, 21062, 1)))]
 
+
+
+
+# General manipulation
+## Add local date
 detections[, date.local := dateandtimeutc]
 setattr(detections$date.local, 'tzone', 'America/New_York')
+
+
+## Rename columns and reorder
 setnames(detections,
          c('dateandtimeutc', 'stationname', 'latitude', 'longitude'),
          c('date.utc', 'station', 'lat', 'long'))
 setorder(detections, date.utc)
 
+
+## Repair station names
+##  Doing this in steps for the sake of (a little bit of?) clarity
+
+### Create a lookup table (key)
+key <- data.table(station = unique(detections$station))
+
+### Remove leading numbers and any apostrophe
+###   Regex plain English: "starts with a 1-2 digit number which may or may not
+###     be followed by 'point number', which is then followed by a space
+###     OR any apostrophe"
+key[, repair := gsub("^\\d{1,2}(\\.\\d)? |'", '', station)]
+
+### Remove "Marshyhope" and "Nanticoke" monikers
+###   Regex plain English: "Marshyhope or Nanticoke, followed by any number of
+###     spaces or slashes, possibly followed by a number then a space
+key[, repair := gsub("(Marshyhope|Nanticoke)[ /]*(\\d )?", '', repair)]
+
+### Remove the word "road" and abbreviations
+key[, repair := gsub(' (Road|Rd.)', '', repair)]
+
+### Fix the regex casualties
+key[, repair := fifelse(grepl('[Cc]onfluence', repair), 'Marshyhope Confluence', repair)]
+key[, repair := fifelse(grepl('mouth', repair), 'Nanticoke Mouth', repair)]
+
+### Final individual changes
+key[, repair := gsub('warf', 'Wharf', repair)]
+key[, repair := fifelse(grepl('VFW', repair), 'VFW Boat Ramp', repair)]
+key[, repair := fifelse(repair == 'Wright', 'Wrights Pier', repair)]
+
+### Make everything have consistent casing
+key[, repair := tools::toTitleCase(repair)]
+
+### Join the lookup table
+detections <- detections[key, on = 'station']
+
+### Complete renaming and drop extra column
+detections[, ':='(station = repair, repair = NULL)]
+
+
+
 # Arrange columns
 detections <- detections[, .(date.utc, date.local, transmitter,
                              station, receiver, lat, long)]
-
 
 
 # Export
